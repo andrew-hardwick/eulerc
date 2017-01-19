39713d39a0fbb074ed113fbcd2fc542d9467ea5c
Andrew <drew.hardwick@gmail.com>
2017-01-19 07:51:00 -0600

    Comments

--- main.c
+++ main.c
@@ -3,23 +3,30 @@
 
 int main()
 {
+  // array that defines the problems to run
   int perf[] = {1, 2};
 
+  // actual and expected result
   int result = 0;
   int expec = 0;
 
+  // function pointer to the functions that compute the actual result
   int (*func)();
 
+  // i is index for for loop. count is number of items in problem array
   int i = 0;
   int count = sizeof(perf) / sizeof(int);
 
   for (i = 0; i < count; i++)
   {
+    // retrieve the problem function and execute it
     func = p001_020_select(perf[i]);
     result = func();
 
+    // retrieve the expected result
     expec = p001_020_expected(perf[i]);
 
+    // output the result in a readable format
     if(expec == result)
     {
       printf("%i: %i is correct.\n", perf[i], result);
--- p001_020.c
+++ p001_020.c
@@ -1,9 +1,13 @@
+// Problem 1
 int p001()
 {
   int out = 0;
+
+  // i is the index value of the for loop
   int i = 0;
   for(i = 0; i < 1000; i++)
   {
+    //sum values that are divisible by 3 and 5
     if(i % 3 == 0 || i % 5 == 0)
     {
       out += i;
@@ -12,24 +16,31 @@ int p001()
   return out;
 }
 
+// Problem 2
 int p002()
 {
   int out = 0;
 
+  // fibonacci variables
   int f1 = 1;
   int f2 = 1; 
 
+  // loop until the sequence is at 4000000
   while (f1 < 4000000){
+    // sum the even entries
     if (f1 % 2 == 0)
     {
       out += f1;
     }
+
+    // compute the next entry in the sequence
     f2 = f1 + f2;
     f1 = f2 - f1;
   }
   return out;
 }
 
+// Provide the expected result for formatting in main
 int p001_020_expected(int index)
 {
   switch(index)
@@ -43,6 +54,7 @@ int p001_020_expected(int index)
   }
 }
 
+// return the function pointer for a specific problem
 int (*p001_020_select(int index))()
 {
   switch (index)
